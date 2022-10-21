8d1acd0a36dd75da5e34b472296048ddd44809ed
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-10-21 10:09:33 +0600

    created two api for sending a spesific news and category news

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ app.use(cors());
 const port = process.env.PORT || 5000;
 
 const categories = require("./data/categories.json");
-// console.log(categories);
+const news = require("./data/news.json");
+// console.log(news);
 
 app.get("/", (req, res) => {
   res.send("Hello world");
@@ -15,6 +16,23 @@ app.get("/", (req, res) => {
 
 app.get("/news-categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } else {
+    const selectedCategoryNews = news.filter((n) => n.category_id === id);
+    res.send(selectedCategoryNews);
+  }
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.filter((n) => n._id === id);
+  console.log(selectedNews);
+  res.send(selectedNews);
 });
 
 app.listen(port, () => {
